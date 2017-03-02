f46bc2047293e0452794356aa613d7c1f03de86a
DimaHmaruk <dhmaruk@gmail.com>
2017-03-02 13:51:27 +0200

    Add Tests

--- test/test.js
+++ test/test.js
@@ -1,69 +1,73 @@
 describe("Tests", function() {
+    var DisplayValue = "123";
+    var action = "+";
+    var newNumber = "1";
+    var buff = 0;
 
-    it("Function 'actions' is ok", function() {
-        var x = "12";
-        var y = "+"
-        actions(x ,y)
-        assert.equal(x, firstValue);
-        assert.equal(y, act);
-    });
-
-    it("Function 'addNumber' is ok", function() {
-        var x = "123";
-        if (display.value !== "0") {
-            var y = display.value;
-        }else{
-            y = "";
-        }
-        addNumber(x);
-        assert.equal((y+x), display.value);
-
-    });
-
-    it("Function 'deleteLast' is ok", function() {
-        var x = "123";
-        deleteLast(x);
+    it("Test function 'deleteLast' with input parameters:figure from the display '" + DisplayValue +
+        "'. Result: Display value = '12' ", function() {
+        display.value = DisplayValue;
+        deleteLast(DisplayValue);
         assert.equal(display.value, "12");
 
     });
 
-    it("Function 'equally' is ok with +", function() {
+    it("Test function 'actions' with input parameters:figure from the display '"+ DisplayValue +
+        "' and selected mathematical operation '" + action + "'. Result: global variable 'firstValue' = '"+
+        DisplayValue + "', global variable 'act' = '" + action +"' ", function() {
+        actions(DisplayValue ,action);
+        assert.equal(DisplayValue, firstValue);
+        assert.equal(action, act);
+    });
+
+    it("Test function 'addNumber' with input parameters:figure from the display '"+ DisplayValue +
+        "' and new number '" + newNumber + "'. Result: Display value = '"+
+        DisplayValue + newNumber +"' ", function() {
+        display.value = DisplayValue;
+        if (DisplayValue !== "0") {
+            buff = DisplayValue;
+        }else{
+            buff = "";
+        }
+        addNumber(newNumber);
+        assert.equal((buff + newNumber), display.value);
+
+    });
+
+    var buff2 = 100;
+    it("Test function 'equally' with action '+', function should be summarized: '" + DisplayValue +
+        "' and '" + buff2 + "', result: '" +(buff2 + +DisplayValue)+"'.", function() {
         act = "+";
-        var x = 1;
-        var y = 1;
-        firstValue = x;
-        equally(y);
-        assert.equal(firstValue, (+x + +y));
+        firstValue = buff2;
+        equally(DisplayValue);
+        assert.equal(firstValue, (+buff2 + (+DisplayValue)));
     });
 
-    it("Function 'equally' is ok with -", function() {
+    it("Test function 'equally' with action '-', function should be subtraction: '" + DisplayValue +
+        "' and '" + buff2 + "', result: '" +(buff2 - +DisplayValue)+"'.", function() {
         act = "-";
-        var x = 3;
-        var y = 2;
-        firstValue = x;
-        equally(y);
-        assert.equal(firstValue, (+x - +y));
+        firstValue = buff2;
+        equally(DisplayValue);
+        assert.equal(firstValue, (+buff2 - (+DisplayValue)));
     });
 
-    it("Function 'equally' is ok with *", function() {
+    it("Test function 'equally' with action '*', function should be multiply: '" + DisplayValue +
+        "' and '" + buff2 + "', result: '" +(buff2 * +DisplayValue)+"'.", function() {
         act = "*";
-        var x = 2;
-        var y = 2;
-        firstValue = x;
-        equally(y);
-        assert.equal(firstValue, (+x * +y));
+        firstValue = buff2;
+        equally(DisplayValue);
+        assert.equal(firstValue, (+buff2 * (+DisplayValue )));
     });
 
-    it("Function 'equally' is ok with /", function() {
+    it("Test function 'equally' with action '/', function should be divide: '" + DisplayValue +
+        "' and '" + buff2 + "', result: '" +(buff2 / +DisplayValue)+"'.", function() {
         act = "/";
-        var x = 4;
-        var y = 2;
-        firstValue = x;
-        equally(y);
-        assert.equal(firstValue, (+x / +y));
+        firstValue = buff2;
+        equally(DisplayValue);
+        assert.equal(firstValue, (+buff2 / (+DisplayValue)));
     });
 
-    it("Function 'clearAlls' is ok", function() {
+    it("Test function 'clearAlls', function must clear the global field 'firstValue' and 'act'", function() {
         firstValue = 1;
         act = "+";
         clearAlls();
